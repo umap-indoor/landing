cecb4903150bf0ee67945ee0c0e7449923dde117
Anna <anna47297@gmail.com>
2022-11-16 16:48:42 +0300

    feat: add menu burger

--- js/main.js
+++ js/main.js
@@ -550,8 +550,32 @@ popupContact.addEventListener('click', (event) => {
 })
 
 
+const BtnMenu = document.querySelector('.btn-menu');
+const Burger = document.querySelector('.burger')
+const BurgerMenu = document.querySelector('.burger-menu');
+const BurgerItem = document.querySelectorAll('.menu__item-link-burger')
+const BurgerShadow = document.querySelector('.burger-shadow')
+const BodyHtml = document.querySelector('html')
+console.log(BodyHtml)
+BtnMenu.addEventListener('click', () => {
+    BurgerMenu.classList.toggle('burger-menu-active');
+    BtnMenu.classList.toggle('btn-menu-active');
+    Burger.classList.toggle('burger-active');
+    BurgerShadow.classList.toggle('burger-shadow-active');
+    BodyHtml.classList.toggle('no-scroll');
 
+    
+})
 
+BurgerItem.forEach(item => {
+    item.addEventListener('click', () => {
+        BurgerMenu.classList.toggle('burger-menu-active');
+        BtnMenu.classList.toggle('btn-menu-active');
+        Burger.classList.toggle('burger-active');
+        BurgerShadow.classList.toggle('burger-shadow-active');
+        BodyHtml.classList.toggle('no-scroll');
+    })
+})
 // const promises = [
 //     new Promise((resolve) => {
 //         setTimeout(() => resolve(1) , 500);
